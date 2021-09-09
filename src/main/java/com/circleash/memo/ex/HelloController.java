638c773c51094210528559e413cb081a70e3ea94
DESKTOP-4GKAT56\JE <circleash92@gmail.com>
2021-09-09 13:23:39 +0900

    [최원제]  - Hello World 입력 확인

--- src/main/java/com/circleash/memo/ex/HelloController.java
+++ src/main/java/com/circleash/memo/ex/HelloController.java
@@ -1,5 +1,14 @@
 package com.circleash.memo.ex;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String helloworld() {
+		return "Hello World";
+	}
 }
